4074e8618078853c25b7407d44377c7931815102
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-03-25 21:14:08 +0200

    update channel container

--- client/src/components/ChanellContainer.jsx
+++ client/src/components/ChanellContainer.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import {Channel, useChatContext, MessageSimple} from 'stream-chat-react';
 
-const ChanellContainer = () => {
+import {ChannelInner, CreateChannel,EditChannel} from './';
+
+const ChanellContainer = ({isCreating,setIsCreating,isEditing,setIsEditing,createType}) => {
+  const {channel}=useChatContext();
+
+  if(isCreating){
+    return(
+        <div className='channel__container'>
+          <CreateChannel createType={createType} setIsCreating={setIsCreating}/>
+
+        </div>
+    )
+  }
+  if(isEditing){
+    return(
+        <div className='channel__container'>
+          <EditChannel setIsEditing={setIsEditing}/>
+
+        </div>
+
+    )
+  }
+
+  const EmptyState=()=>{
+    <div className='channel-empty__container'>
+      <p className='channel-empty__first'>This  channel is empty. start typing here</p>
+      <p className='channel-empty__second'>Send messages and more</p>
+
+    </div>
+  }
+
   return (
-    <div>ChanellContainer</div>
+    <div className='channel__container'>
+        <Channel
+          EmptyStateIndicator={EmptyState}
+          Message={(messageProps,i)=> <MessageSimple key={i} {...messageProps} />}
+        >
+            <ChannelInner setIsEditing={setIsEditing}/>
+        </Channel>
+    </div>
   )
 }
 
